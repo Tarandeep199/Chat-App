e11e66a1170aeb84011ce5cfe46e4fc4757b140a
Tarandeep199 <tarandeepsingh199@gmail.com>
2021-02-25 21:26:39 -0800

    Established socket communication

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
-const { allowedNodeEnvironmentFlags } = require('process')
+const socketio = require('socket.io')
+
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port,()=>{
+let count = 0
+
+io.on('connection',(socket)=>{
+    console.log('New WebSocket connection')
+    socket.emit('countUpdated',count)
+
+    socket.on('increment',()=>{
+        count++
+        // socket.emit('countUpdated',count)
+        io.emit('countUpdated',count)   //will emit to all connections
+
+    })
+
+})
+
+server.listen(port,()=>{
     console.log('Server up on port '+ port)
 })
+ 
