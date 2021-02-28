7db4c77db4de455e09058f39989b35afd246de66
Tarandeep199 <tarandeepsingh199@gmail.com>
2021-02-27 17:18:15 -0800

    Implemented location sharing feature

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,29 @@
+
 const socket = io()
 
-socket.on('countUpdated',(count)=>{
-    console.log('Count has been updated!',count)
+
+
+socket.on('message',(message)=>{    //listens for welcome event sent from server
+    console.log(message)
 })
 
-document.querySelector('#increment').addEventListener('click',()=>{
-    console.log('button clicked')
-    socket.emit('increment')
+document.querySelector('#Message-form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage',message)  //sends over message to server 
+
 })
+
+document.querySelector('#send-location').addEventListener('click',()=>{
+    if(!navigator.geolocation){
+        return alert('geolocation not supported by browser')
+    }    
+    navigator.geolocation.getCurrentPosition((position)=>{
+        socket.emit('sendLocation',{
+            latitude: position.coords.latitude,
+            longitude:position.coords.longitude
+        })
+    })
+})
+
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const { WebSocketShard } = require('discord.js')
+
 
 
 const app = express()
@@ -14,22 +16,31 @@ const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
+
 
 io.on('connection',(socket)=>{
     console.log('New WebSocket connection')
-    socket.emit('countUpdated',count)
 
-    socket.on('increment',()=>{
-        count++
-        // socket.emit('countUpdated',count)
-        io.emit('countUpdated',count)   //will emit to all connections
+    socket.emit('message','Welcome')    //sends message from server to the clienT
 
+
+    socket.broadcast.emit('message', 'new user has joined')
+
+    socket.on('sendMessage',(message)=>{    //server listens for send message from chat.js
+        io.emit('message',message)
+    })
+
+    socket.on('disconnect',()=>{
+        io.emit('message','user has left')
+    })
+
+    socket.on('sendLocation',(location)=>{
+        io.emit('message',`https://www.google.com/maps/@${location.latitude},${location.longitude}`)
     })
 
 })
 
 server.listen(port,()=>{
-    console.log('Server up on port '+ port)
+    console.log(`Server up on port ${port}`)
 })
  
